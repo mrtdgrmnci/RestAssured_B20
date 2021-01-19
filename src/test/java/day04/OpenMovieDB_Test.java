f59719466d415f1d24386de1a54da589ba8b31a4
Murat <degirmenci_murat@yahoo.com>
2021-01-19 18:50:44 -0500

    Initial commit

---
+++ src/test/java/day04/OpenMovieDB_Test.java
@@ -0,0 +1,74 @@
+package day04;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.*;
+import io.restassured.http.ContentType;
+import static io.restassured.RestAssured.* ;
+import static org.hamcrest.Matchers.* ;
+
+public class OpenMovieDB_Test {
+
+    // http://www.omdbapi.com/?t=The Orville&apiKey=5b5d0fe8
+
+    @BeforeAll
+    public static void setUp(){
+        RestAssured.baseURI = "http://www.omdbapi.com" ;
+    }
+    @AfterAll
+    public static void tearDown(){
+        reset();
+    }
+
+    @DisplayName("Test Search Movie or OpenMovieDB Test")
+    @Test
+    public void testMovie(){
+
+        given()
+                .queryParam("apiKey","5b5d0fe8" )
+                .queryParam("t","The Orville").
+
+        when()
+                .get().prettyPeek().  // our request URL is already complete , do not need to add anything here
+        then()
+                .statusCode( is(200) )
+                .contentType( ContentType.JSON )
+                .body("Title" , is("The Orville"))
+                .body("Ratings[0].Source" , is("Internet Movie Database") )
+
+                ;
+
+
+    }
+
+
+    @DisplayName("Getting the log of request and response")
+    @Test
+    public void testSendingRequestAndGetTheLog(){
+
+        given()
+                .queryParam("apiKey","5b5d0fe8" )
+                .queryParam("t","John Wick")
+                // logging the request should be in given section
+                .log().all().
+//                .log().uri().
+//                  .log().params().
+        when()
+                .get().
+        then()
+                // logging the response should be in then section
+//                .log().all()
+//                .log().status()
+//                .log().body()
+                .log().ifValidationFails()
+                .statusCode(  is(200)  )
+                .body("Plot", containsString("ex-hit-man") )
+                // second Ratings source is Rotten Tomato
+                .body("Ratings[1].Source",is("Rotten Tomatoes") )
+
+                ;
+
+
+    }
+
+
+}
---
+++ src/test/java/day11/xml_extra/Item.java
@@ -0,0 +1,18 @@
+package day11.xml_extra;
+
+import lombok.*;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Getter @Setter @ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@XmlRootElement
+public class Item {
+
+    private int id ;
+    private String name;
+    private String gender;
+    private long phone ;
+
+}
